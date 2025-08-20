ad5a88c4dc9cd527c6063e092f9b8e037067d0b7
Mike McGee <mmcgee738@gmail.com>
2025-08-19 21:28:06 -0500

    destroy method on recipe views

--- coffeecrewapi/views/recipe.py
+++ coffeecrewapi/views/recipe.py
@@ -101,3 +101,18 @@ class Recipes(ViewSet):
     recipe.save()
     
     return Response({}, status=status.HTTP_204_NO_CONTENT)
+  
+  def destroy(self, request, pk=None):
+    """Delete a recipe object
+    
+    Returns status code 204 - no content on success"""
+    
+    try:
+      recipe = Recipe.objects.get(pk=pk)
+      recipe.delete()
+      
+      return Response({}, status=status.HTTP_204_NO_CONTENT)
+    except Recipe.DoesNotExist as ex:
+      return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as ex:
+      return Response({"message": ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
