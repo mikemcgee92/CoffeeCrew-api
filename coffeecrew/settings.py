0ed3cf08365734d694c310add268e9da5e5f833c
Mike McGee <mmcgee738@gmail.com>
2025-09-15 18:48:54 -0500

    updated settings

--- coffeecrew/settings.py
+++ coffeecrew/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = coffeecrewapi_secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
