daa7c9630a41a8b45a88129ac57a517b66d8f095
Mike McGee <mmcgee738@gmail.com>
2025-09-23 19:58:56 -0500

    updated user info views

--- coffeecrewapi/views/__init__.py
+++ coffeecrewapi/views/__init__.py
@@ -2,4 +2,5 @@ from .recipe import Recipes
 from .category import Categories
 from .ingredient import Ingredients
 from .completed_order import CompletedOrders
+from .user_info import UserInfos
 from .square import client, get_orders
