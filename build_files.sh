4ebaafe1dbc6d8d73485069e48f5480e08a8d91f
Mike McGee <mmcgee738@gmail.com>
2025-10-06 21:14:31 -0500

    fixed deploy settings and wsgi

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
-pip install -r requirements.txt
-python 3.9 manage.py collectstatic --no-input --clear
+#!/bin/bash
+export PATH=$PATH:/vercel/.local/bin
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --no-input --clear
