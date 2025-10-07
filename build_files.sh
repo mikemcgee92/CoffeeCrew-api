ddf92680b9e19b6fb52dab4f12dac6fb3d4e09ec
Mike McGee <mmcgee738@gmail.com>
2025-10-06 21:17:30 -0500

    testing new deploy script

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export PATH=$PATH:/vercel/.local/bin
+echo "Installing dependencies..."
 python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install --no-cache-dir -r requirements.txt
+
+echo "Running collectstatic..."
 python3.9 manage.py collectstatic --no-input --clear
