b1f72ef67625ff41320a1f086445289c10363f0f
Mike McGee <mmcgee738@gmail.com>
2025-10-06 21:25:26 -0500

    testing new deploy

---
+++ vercel_app.py
@@ -0,0 +1,9 @@
+"""
+WSGI config for Vercel deployment
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffeecrewproject.settings')
+
+app = get_wsgi_application()
