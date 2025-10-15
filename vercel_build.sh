51e35a016c6930f406ac40d597a20c91e1ab3103
Mike McGee <mmcgee738@gmail.com>
2025-10-15 17:26:44 -0500

    updated build script

--- vercel_build.sh
+++ vercel_build.sh
@@ -10,5 +10,8 @@ export DJANGO_SETTINGS_MODULE=coffeecrew.production_settings
 echo "Running Django migrations..."
 python manage.py migrate --noinput
 
+echo "Loading initial data..."
+python manage.py loaddata category ingredient recipe
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
