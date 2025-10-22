34b00400e18c7ed7695ed720bc1a7cec8f1ec34d
Mike McGee <mmcgee738@gmail.com>
2025-10-22 05:59:39 -0500

    changed CORS decorator

--- coffeecrewapi/middleware.py
+++ coffeecrewapi/middleware.py
@@ -4,16 +4,25 @@ from django.http import HttpResponse
 def cors_allow_all(view_func):
     @wraps(view_func)
     def wrapped_view(*args, **kwargs):
-        if args and hasattr(args[0], 'method') and args[0].method == 'OPTIONS':
-            response = HttpResponse()
-            response['Content-Length'] = '0'
-            response.status_code = 200
-        else:
-            response = view_func(*args, **kwargs)
+        if args and hasattr(args[0], 'method'):
+            request = args[0]
+            if request.method == 'OPTIONS':
+                response = HttpResponse()
+                response['Content-Length'] = '0'
+                response.status_code = 204  # No Content
+            else:
+                response = view_func(*args, **kwargs)
 
-        response['Access-Control-Allow-Origin'] = '*'
-        response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
-        response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
-        response['Access-Control-Max-Age'] = '86400'
-        return response
+            # Always add CORS headers
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+            response['Access-Control-Allow-Headers'] = '*'  # Allow all headers
+            response['Access-Control-Allow-Credentials'] = 'true'
+            response['Access-Control-Max-Age'] = '86400'  # 24 hours
+            
+            # Add Vary header to prevent caching issues
+            response['Vary'] = 'Origin'
+            
+            return response
+        return view_func(*args, **kwargs)
     return wrapped_view
